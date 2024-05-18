dcc2846b8f9ac00deac66277eb38b5eb903cb093
Muhammad Taha Raees <raeesmuhammadtaha@gmail.com>
2024-05-18 07:27:41 +0100

    12

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -1,5 +1,4 @@
 import { getOrders } from "@/lib/actions/actions";
-
 import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 
@@ -12,24 +11,21 @@ const Orders = async () => {
   return (
     <div className="px-10 py-5 max-sm:px-3">
       <p className="text-heading3-bold my-10">Your Orders</p>
-      {!orders ||
-        (orders.length === 0 && (
-          <p className="text-body-bold my-5">You have no orders yet.</p>
-        ))}
+      {!orders || (orders.length === 0 && (
+        <p className="text-body-bold my-5">You have no orders yet.</p>
+      ))}
 
       <div className="flex flex-col gap-10">
         {orders?.map((order: OrderType) => (
-          <div className="flex flex-col gap-8 p-4 hover:bg-grey-1">
+          <div key={order._id} className="flex flex-col gap-8 p-4 hover:bg-grey-1">
             <div className="flex gap-20 max-md:flex-col max-md:gap-3">
               <p className="text-base-bold">Order ID: {order._id}</p>
-              <p className="text-base-bold">
-                Total Amount: ${order.totalAmount}
-              </p>
+              <p className="text-base-bold">Total Amount: ${order.totalAmount}</p>
             </div>
 
             <div className="flex flex-col gap-5">
               {order.products.map((orderItem: OrderItemType) => (
-                <div className="flex gap-4">
+                <div key={orderItem.product._id} className="flex gap-4">
                   <Image
                     src={orderItem.product.media[0]}
                     alt={orderItem.product.title}
@@ -39,34 +35,23 @@ const Orders = async () => {
                   />
                   <div className="flex flex-col justify-between">
                     <p className="text-small-medium">
-                      Title:{" "}
-                      <span className="text-small-bold">
-                        {orderItem.product.title}
-                      </span>
+                      Title: <span className="text-small-bold">{orderItem.product.title}</span>
                     </p>
                     {orderItem.color && (
                       <p className="text-small-medium">
-                        Color:{" "}
-                        <span className="text-small-bold">
-                          {orderItem.color}
-                        </span>
+                        Color: <span className="text-small-bold">{orderItem.color}</span>
                       </p>
                     )}
                     {orderItem.size && (
                       <p className="text-small-medium">
-                        Size:{" "}
-                        <span className="text-small-bold">
-                          {orderItem.size}
-                        </span>
+                        Size: <span className="text-small-bold">{orderItem.size}</span>
                       </p>
                     )}
                     <p className="text-small-medium">
-                      Unit price:{" "}
-                      <span className="text-small-bold">{orderItem.product.price}</span>
+                      Unit price: <span className="text-small-bold">${orderItem.product.price}</span>
                     </p>
                     <p className="text-small-medium">
-                      Quantity:{" "}
-                      <span className="text-small-bold">{orderItem.quantity}</span>
+                      Quantity: <span className="text-small-bold">{orderItem.quantity}</span>
                     </p>
                   </div>
                 </div>
